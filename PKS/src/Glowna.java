07266109de5f2c56cfcf1067d59e3d89f78fd7bf
KubaPerkowski19 <84773604+KubaPerkowski19@users.noreply.github.com>
2022-11-27 16:07:37 +0100

    Update Glowna.java

--- PKS/src/Glowna.java
+++ PKS/src/Glowna.java
@@ -1,11 +1,12 @@
 public class Glowna {
-    static int ilosc_autobusow =100000;
+    static int ilosc_autobusow =100;
     static int ilosc_miejsc_parkingowych =5;
+    static int ilosc_kierowcow = 100;
     static Stacja_PKS stacjaPKS;
     public Glowna(){
     }
     public static void main(String[] args) {
-        stacjaPKS =new Stacja_PKS(ilosc_miejsc_parkingowych, ilosc_autobusow);
+        stacjaPKS =new Stacja_PKS(ilosc_miejsc_parkingowych, ilosc_autobusow, ilosc_kierowcow);
         for(int i = 0; i< ilosc_autobusow; i++)
             new Autobus(i,2000, stacjaPKS).start();
     }
